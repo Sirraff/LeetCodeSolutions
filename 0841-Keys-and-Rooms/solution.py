113bcebe35a996835490350be7d8d590136f9724
Raf Reis <rafael.ls.reis@gmail.com>
2023-07-15 13:32:04 -0700

    Create solution.py

---
+++ 0841-Keys-and-Rooms/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        visited = [False] * len(rooms)
+        visited[0] = True
+        
+        queue = deque([0])
+        
+        while queue:
+            room = queue.popleft()
+            for key in rooms[room]:
+                if not visited[key]:
+                    visited[key] = True
+                    queue.append(key)
+                    
+        return all(visited)
